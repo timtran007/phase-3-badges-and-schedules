4f70e4218753e38b57cabbb2d8671ca176c151d6
Tim <timtran007@gmail.com>
2022-12-29 11:25:10 -0600

    commit tree e7bbfa20482b4d4a89418f8b42633f7bd1583676

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+require 'pry'
+def badge_maker (name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (names)
+    names.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms (names)
+    room_number = 0
+    names.map do |name|
+        room_number +=1
+        "Hello, #{name}! You'll be assigned to room #{room_number}!"
+    end
+end
+
+def printer (attendees)
+    room_number = 0
+    attendees
+    .map do |attendee|
+        puts "Hello, my name is #{attendee}."
+    end
+    attendees.map do |attendee|
+        room_number +=1
+        puts "Hello, #{attendee}! You'll be assigned to room #{room_number}!"
+    end
+end
+
+
